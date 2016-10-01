17527b912c5baa7afc6f6b37bdbe31718a93d4aa
Lee Saville <l.w.saville@gmail.com>
2016-10-01 16:44:49 -0600

    Refactor to DRY out the common lines in .all and .find

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,8 +15,7 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    self.database
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -24,9 +23,15 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    self.database
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
     Task.new(task)
   end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
 end
