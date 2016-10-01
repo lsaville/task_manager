8dc7bd19e2330efc1e6f02c31bd2c63fd33f36d3
Lee Saville <l.w.saville@gmail.com>
2016-10-01 16:31:27 -0600

    Add ivar @id to Task inititalize. Add to attr_reader

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,13 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
